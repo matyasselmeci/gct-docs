9cc901885b9bfe137b01e9677cc5cab3ebb97e1f
Matyas Selmeci <matyas@cs.wisc.edu>
2019-05-17 19:44:17 -0500

    fixup! Add travis

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-exec sudo docker run --rm=false -v `pwd`:/gct-docs:rwZ asciidoctor/docker-asciidoctor /bin/bash -xe /gct-docs/travis-ci/run_inside_docker.sh
-
+exec sudo docker run --rm=false -v `pwd`:/gct-docs:rw asciidoctor/docker-asciidoctor /bin/bash -xe /gct-docs/travis-ci/run_inside_docker.sh
