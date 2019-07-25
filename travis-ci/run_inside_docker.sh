a86ab4d0a1ea8085d73924f082ee4f1a6f9ef885
Matyas Selmeci <matyas@cs.wisc.edu>
2019-07-25 10:49:10 -0500

    Make sure some htmls were generated

--- travis-ci/run_inside_docker.sh
+++ travis-ci/run_inside_docker.sh
@@ -2,10 +2,22 @@
 
 apk add --no-cache ghostscript
 cd /gct-docs/latest
+# There's no make clean so do it by hand
+set +x
 find . -type f -name \*.adoc | \
     while read line; do
-        rm -f ${line%.adoc}.html
+        html=${line%.adoc}.html
+        rm -vf "$html"
 done
+set -x
 make
+# Validate
+set +x
+set +e
+find . -type f -name \*.adoc | \
+    while read line; do
+        html=${line%.adoc}.html
+        ls -l "$html"
+done
 
 # vim:et:sw=4:sts=4:ts=8
