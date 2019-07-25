606412b5d9e20f17b96bd5f84c1797427b044639
Matyas Selmeci <matyas@cs.wisc.edu>
2019-07-25 10:34:44 -0500

    Clean html before rebuilding

--- travis-ci/run_inside_docker.sh
+++ travis-ci/run_inside_docker.sh
@@ -2,6 +2,10 @@
 
 apk add --no-cache ghostscript
 cd /gct-docs/latest
+find . -type f -name \*.adoc | \
+    while read line; do
+        rm -f ${line%.adoc}.html
+done
 make
 
 # vim:et:sw=4:sts=4:ts=8
